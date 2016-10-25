fc24f6275a90beaa83b269014ee5094028fd2d9a
Takuya Kitazawa <k.takuti@gmail.com>
2016-10-25 10:52:44 +0900

    Implement additional ranking measures
    * Precision
    * MAP
    * AUC
    * MRR
    * MPR
    * NDCG

--- src/utils/measures.jl
+++ src/utils/measures.jl
@@ -12,8 +12,80 @@ function count_true_positive{T}(truth::Array{T}, recommend::Array{T})
     tp
 end
 
+# Recall@k
 function recall{T}(truth::Array{T}, recommend::Array{T}, k::Int)
     count_true_positive(truth, recommend[1:k]) / length(truth)
+end
+
+# Precision@k
+function precision{T}(truth::Array{T}, recommend::Array{T}, k::Int)
+    count_true_positive(truth, recommend[1:k]) / k
+end
+
+# Mean Average Precision
+function map{T}(truth::Array{T}, recommend::Array{T}, k::Int=0)
+    tp = accum = 0
+    n_recommend = length(recommend)
+
+    for n = 1:n_recommend
+        if findfirst(truth, recommend[n]) != 0
+            tp += 1
+            accum += tp / n
+        end
+    end
+
+    accum / length(truth)
+end
+
+# Area under the ROC curve
+function auc{T}(truth::Array{T}, recommend::Array{T}, k::Int=0)
+    tp = correct = 0
+    for r in recommend
+        if findfirst(truth, r) != 0
+            # keep track number of tp placed before
+            tp += 1
+        else
+            correct += tp
+        end
+    end
+    # number of all possible tp-fp pairs
+    pairs = tp * (length(recommend) - tp)
+    correct / pairs
+end
+
+# Mean Reciprocal Rank
+function mrr{T}(truth::Array{T}, recommend::Array{T}, k::Int=0)
+    n_recommend = length(recommend)
+    for n = 1:n_recommend
+        if findfirst(truth, recommend[n]) != 0
+            return 1 / n
+        end
+    end
+    return 0
+end
+
+# Mean Percentile Rank
+function mpr{T}(truth::Array{T}, recommend::Array{T}, k::Int=0)
+    accum = 0
+    n_recommend = length(recommend)
+    for t in truth
+        r = (findfirst(recommend, t) - 1) / n_recommend
+        accum += r
+    end
+    accum * 100 / length(truth)
+end
+
+# Normalized Discounted Cumulative Gain
+function ndcg{T}(truth::Array{T}, recommend::Array{T}, k::Int)
+    dcg = idcg = 0
+    for n = 1:k
+        d = 1 / log2(n + 1)
+        if findfirst(truth, recommend[n]) != 0
+            dcg += d
+        end
+        idcg += d
+    end
+    dcg / idcg
 end
 
 end # module Measures
--- test/utils/measures.jl
+++ test/utils/measures.jl
@@ -6,7 +6,82 @@ function test_recall()
     actual = Measures.recall(truth, recommend, k)
     expected = 0.333
     eps = 0.001
+
+    @test abs(actual - expected) < eps
+end
+
+function test_precision()
+    truth = [1, 2, 4]
+    recommend = [1, 3, 2, 6, 4, 5]
+    k = 2
+
+    actual = Measures.precision(truth, recommend, k)
+    expected = 0.5
+    eps = 0.001
+
+    @test abs(actual - expected) < eps
+end
+
+function test_map()
+    truth = [1, 2, 4]
+    recommend = [1, 3, 2, 6, 4, 5]
+
+    actual = Measures.map(truth, recommend)
+    expected = 0.756
+    eps = 0.001
+
+    @test abs(actual - expected) < eps
+end
+
+function test_auc()
+    truth = [1, 2, 4]
+    recommend = [1, 3, 2, 6, 4, 5]
+
+    actual = Measures.auc(truth, recommend)
+    expected = 0.667
+    eps = 0.001
+
+    @test abs(actual - expected) < eps
+end
+
+function test_mrr()
+    truth = [1, 2, 4]
+    recommend = [1, 3, 2, 6, 4, 5]
+
+    actual = Measures.mrr(truth, recommend)
+    expected = 1.0
+    eps = 0.001
+
+    @test abs(actual - expected) < eps
+end
+
+function test_mpr()
+    truth = [1, 2, 4]
+    recommend = [1, 3, 2, 6, 4, 5]
+
+    actual = Measures.mpr(truth, recommend)
+    expected = 33.333
+    eps = 0.001
+
+    @test abs(actual - expected) < eps
+end
+
+function test_ndcg()
+    truth = [1, 2, 4]
+    recommend = [1, 3, 2, 6, 4, 5]
+    k = 2
+
+    actual = Measures.ndcg(truth, recommend, k)
+    expected = 0.613
+    eps = 0.001
+
     @test abs(actual - expected) < eps
 end
 
 test_recall()
+test_precision()
+test_map()
+test_auc()
+test_mrr()
+test_mpr()
+test_ndcg()
