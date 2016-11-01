e50ff284e9e6069100dc7331af4f3bf352d7875c
Takuya Kitazawa <k.takuti@gmail.com>
2016-11-02 06:42:01 +0900

    Typo

--- src/recommender.jl
+++ src/recommender.jl
@@ -4,7 +4,7 @@ export predict, ranking
 abstract Recommender
 
 function predict(recommender::Recommender, u::Int, i::Int)
-    error("prdict is not implemented for recommender type $(typeof(recommender))")
+    error("predict is not implemented for recommender type $(typeof(recommender))")
 end
 
 # Return a ranking score of item i for user u
