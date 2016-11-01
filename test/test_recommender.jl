ee0a45c8ebb0e7e9733949201df405c7f6ba14d5
Takuya Kitazawa <k.takuti@gmail.com>
2016-11-02 07:04:47 +0900

    Add NotImplementedError test for ranking method

--- test/test_recommender.jl
+++ test/test_recommender.jl
@@ -6,6 +6,7 @@ function test_not_implemented_error()
     println("-- Testing Recommender base type")
     recommender = Foo(sparse([1 2 3; 4 5 6]))
     @test_throws ErrorException predict(recommender, 1, 1)
+    @test_throws ErrorException ranking(recommender, 1, 1)
 end
 
 test_not_implemented_error()
