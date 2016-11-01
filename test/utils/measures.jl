dc12532fd6cb739df3bccad763e3f09442ff0756
Takuya Kitazawa <k.takuti@gmail.com>
2016-11-01 22:43:38 +0900

    Use global variable in test of ranking measures

--- test/utils/measures.jl
+++ test/utils/measures.jl
@@ -1,8 +1,8 @@
-function test_recall()
-    truth = [1, 2, 4]
-    recommend = [1, 3, 2, 6, 4, 5]
-    k = 2
+const truth = [1, 2, 4]
+const recommend = [1, 3, 2, 6, 4, 5]
+const k = 2
 
+function test_recall()
     actual = Measures.recall(truth, recommend, k)
     expected = 0.333
     eps = 0.001
@@ -11,10 +11,6 @@ function test_recall()
 end
 
 function test_precision()
-    truth = [1, 2, 4]
-    recommend = [1, 3, 2, 6, 4, 5]
-    k = 2
-
     actual = Measures.precision(truth, recommend, k)
     expected = 0.5
     eps = 0.001
@@ -23,9 +19,6 @@ function test_precision()
 end
 
 function test_map()
-    truth = [1, 2, 4]
-    recommend = [1, 3, 2, 6, 4, 5]
-
     actual = Measures.map(truth, recommend)
     expected = 0.756
     eps = 0.001
@@ -34,9 +27,6 @@ function test_map()
 end
 
 function test_auc()
-    truth = [1, 2, 4]
-    recommend = [1, 3, 2, 6, 4, 5]
-
     actual = Measures.auc(truth, recommend)
     expected = 0.667
     eps = 0.001
@@ -45,9 +35,6 @@ function test_auc()
 end
 
 function test_mrr()
-    truth = [1, 2, 4]
-    recommend = [1, 3, 2, 6, 4, 5]
-
     actual = Measures.mrr(truth, recommend)
     expected = 1.0
     eps = 0.001
@@ -56,9 +43,6 @@ function test_mrr()
 end
 
 function test_mpr()
-    truth = [1, 2, 4]
-    recommend = [1, 3, 2, 6, 4, 5]
-
     actual = Measures.mpr(truth, recommend)
     expected = 33.333
     eps = 0.001
@@ -67,10 +51,6 @@ function test_mpr()
 end
 
 function test_ndcg()
-    truth = [1, 2, 4]
-    recommend = [1, 3, 2, 6, 4, 5]
-    k = 2
-
     actual = Measures.ndcg(truth, recommend, k)
     expected = 0.613
     eps = 0.001
