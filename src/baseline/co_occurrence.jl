31217419fe9609e9c0ba6b708bf2ad905c86cb4e
Takuya Kitazawa <k.takuti@gmail.com>
2016-11-02 16:37:33 +0900

    Get reference vector at the beginning of instansiation on CoOccurence

--- src/baseline/co_occurrence.jl
+++ src/baseline/co_occurrence.jl
@@ -2,11 +2,14 @@ export CoOccurrence
 
 immutable CoOccurrence <: Recommender
     m::SparseMatrixCSC
-    i::Int
+    v_ref::SparseVector
+end
+
+CoOccurrence(m::SparseMatrixCSC, i::Int) = begin
+    CoOccurrence(m, m[:, i])
 end
 
 function ranking(recommender::CoOccurrence, u::Int, i::Int)
-    v_ref = recommender.m[:, recommender.i]
     v = recommender.m[:, i]
-    v_ref[(v_ref .> 0) & (v .> 0)].n / nnz(v_ref) * 100.0
+    recommender.v_ref[(recommender.v_ref .> 0) & (v .> 0)].n / nnz(recommender.v_ref) * 100.0
 end
