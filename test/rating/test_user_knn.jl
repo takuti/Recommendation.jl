99510889f87e6b46ed4dadb962969fcf8d732dc8
Takuya Kitazawa <k.takuti@gmail.com>
2016-11-04 23:48:32 +0900

    Add normalized UserKNN test cases

--- test/rating/test_user_knn.jl
+++ test/rating/test_user_knn.jl
@@ -14,6 +14,16 @@ function test_user_knn()
     @test rec[1] == ("item8" => 3.0)
     @test rec[2] == ("item5" => 3.0)
     @test rec[3] == ("item6" => 2.0)
+
+    recommender = UserKNN(sparse(m), 1, is_normalized=true);
+
+    @test_approx_eq_eps recommender.corr[1, 2] 0.447 1e-3
+    @test_approx_eq_eps recommender.corr[2, 3] -0.693 1e-3
+
+    rec = execute(recommender, 1, 3, ["item$(i)" for i in 1:8])
+    @test rec[1] == ("item8" => 3.0)
+    @test rec[2] == ("item5" => 3.0)
+    @test rec[3] == ("item4" => 2.2)
 end
 
 test_user_knn()
