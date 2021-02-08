6febdff8b3d05478df9f8a49868697966a552ee8
Takuya Kitazawa <k.takuti@gmail.com>
2021-02-09 08:12:32 +0900

    Add cross validation test cases with AccuracyMetrics

--- test/evaluation/test_cross_validation.jl
+++ test/evaluation/test_cross_validation.jl
@@ -1,19 +1,48 @@
-function test_cross_validation()
-    println("-- Testing cross validation")
+function test_cross_validation_accuracy()
+    println("-- Testing cross validation with accuracy metrics")
+
+    m = [NaN 3 NaN 1 2 1 NaN 4
+         1 2 NaN NaN 3 2 NaN 3
+         NaN NaN NaN NaN NaN NaN NaN NaN
+         NaN 2 3 3 NaN 5 NaN 1]
+    data = DataAccessor(m)
+
+    fold = 5
+
+    # MF(data, 2)
+    @test cross_validation(fold, MAE, MF, data, 2) <= 0.5
+
+    # UserMean(data)
+    @test cross_validation(fold, MAE, UserMean, data) <= 2.5
+
+    # ItemMean(data)
+    @test cross_validation(fold, MAE, ItemMean, data) <= 2.5
+
+    # UserKNN(data, 2, true)
+    @test cross_validation(fold, MAE, UserKNN, data, 2, true) <= 0.5
+end
+
+function test_cross_validation_ranking()
+    println("-- Testing cross validation with ranking metrics")
 
     m = [NaN 3 NaN 1 2 1 NaN 4
          1 2 NaN NaN 3 2 NaN 3
          NaN 2 3 3 NaN 5 NaN 1]
     data = DataAccessor(m)
 
-    # 5-fold, top-4 recommendation with MF(data, 2)
-    @test cross_validation(5, Recall, 4, MF, data, 2) <= 0.5
+    # 5-fold, top-4 recommendation
+    fold = 5
+    k = 4
+
+    # MF(data, 2)
+    @test cross_validation(fold, Recall, k, MF, data, 2) <= 0.5
 
     # MostPopular(data)
-    @test cross_validation(5, Recall, 4, MostPopular, data) <= 0.5
+    @test cross_validation(fold, Recall, k, MostPopular, data) <= 0.5
 
     # UserKNN(data, 2, true)
-    @test cross_validation(5, Recall, 4, UserKNN, data, 2, true) <= 0.5
+    @test cross_validation(fold, Recall, k, UserKNN, data, 2, true) <= 0.5
 end
 
-test_cross_validation()
+test_cross_validation_accuracy()
+test_cross_validation_ranking()
