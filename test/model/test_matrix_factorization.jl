90b06134a79021c6922d8ea1150b764653a9b525
Takuya Kitazawa <k.takuti@gmail.com>
2021-12-30 07:46:22 -0800

    Make `MF` alias of `MatrixFactorization` fo readability

--- test/model/test_matrix_factorization.jl
+++ test/model/test_matrix_factorization.jl
@@ -1,12 +1,10 @@
-function test_mf()
-    println("-- Testing MF-based recommender")
-
+function run(recommender::Type{T}) where {T<:Recommender}
     m = [NaN 3 NaN 1 2 1 NaN 4
          1 2 NaN NaN 3 2 NaN 3
          NaN 2 3 3 NaN 5 NaN 1]
     data = DataAccessor(m)
 
-    recommender = MF(data, 2)
+    recommender = recommender(data, 2)
     build!(recommender, learning_rate=15e-4, max_iter=100)
 
     # top-4 recommantion list should be same as CF/SVD-based recommender
@@ -14,4 +12,15 @@ function test_mf()
     @test Set([first(r) for r in rec]) == Set([2, 5, 6, 8])
 end
 
+function test_mf()
+    println("-- Testing MF-based (aliased) recommender")
+    run(MF)
+end
+
+function test_matrix_factorization()
+    println("-- Testing Matrix Factorization-based recommender")
+    run(MatrixFactorization)
+end
+
 test_mf()
+test_matrix_factorization()
