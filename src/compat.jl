16607626c2c62ae19196f2922dcb63e6613b3bf4
Takuya Kitazawa <k.takuti@gmail.com>
2022-02-03 07:22:20 -0800

    Remove the use of unnecessary intermediate function in compat

--- src/compat.jl
+++ src/compat.jl
@@ -1,8 +1,6 @@
 export build!
 
-function bridge_fit!(recommender::Recommender; kwargs...)
+function build!(recommender::Recommender; kwargs...)
     @warn "`build!`` is deprecated and renamed to `fit!`"
     fit!(recommender; kwargs...)
 end
-
-build!(recommender::Recommender; kwargs...) = bridge_fit!(recommender; kwargs...)
