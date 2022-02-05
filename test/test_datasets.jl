6d027989d12c700f3c9eea03a54bf366c61b8760
Takuya Kitazawa <k.takuti@gmail.com>
2022-02-05 08:45:35 -0800

    Implement data file downloader utility

--- test/test_datasets.jl
+++ test/test_datasets.jl
@@ -1,18 +1,28 @@
+function test_get_data_home()
+    dir = mktempdir()
+    println("-- Testing to get a data home with a temp directory: $dir")
+    ENV["JULIA_RECOMMENDATION_DATA"] = dir
+    @test get_data_home() == dir
+end
+
+function test_download_file()
+    # https://archive.ics.uci.edu/ml/datasets/iris
+    iris_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+    path = download_file(iris_url)
+    @test isfile(path)
+end
+
 function test_load_movielens_100k()
-    println("-- Testing to create and load a mock MovieLens 100k data file")
-    path, io = mktemp()
-
-    write(io, "1\t1\t3\n")
-    write(io, "1\t1682\t2\n")
-    write(io, "943\t1\t4\n")
-    flush(io)
-
+    path = joinpath(tempname(), "u.data")
+    println("-- Testing to download and read the MovieLens 100k data file: $path")
     data = load_movielens_100k(path)
 
-    @test data.R[1, 1] == 3.0
-    @test data.R[1, 1682] == 2.0
-    @test data.R[943, 1] == 4.0
+    @test data.R[196, 242] == 3.0
+    @test data.R[186, 302] == 3.0
+    @test data.R[22, 377] == 1.0
     @test data.R[2, 2] == 0.0
 end
 
+test_get_data_home()
+test_download_file()
 test_load_movielens_100k()
