c5b2a882b8076333a3e28cedc7c61de1920396c7
Takuya Kitazawa <k.takuti@gmail.com>
2022-02-05 06:25:46 -0800

    Add docstring to load_movielens_100k

--- src/datasets.jl
+++ src/datasets.jl
@@ -1,5 +1,10 @@
 export load_movielens_100k
 
+"""
+    load_movielens_100k(path::String)
+
+Read user-item-rating triples from a locally saved `u.data` TSV file downloaded from [MovieLens 100k](https://grouplens.org/datasets/movielens/100k/), and convert them into a `DataAccessor` instance.
+"""
 function load_movielens_100k(path::String)
     n_user = 943
     n_item = 1682
