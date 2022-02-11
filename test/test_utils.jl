76a930f4a131e1a22678e1effafa0fbd07ae3468
Takuya Kitazawa <k.takuti@gmail.com>
2022-02-11 05:50:57 -0800

    Tweak onehot value/vector encoding test cases

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -1,16 +1,27 @@
 function test_onehot_value()
     println("-- Testing onehot encoding of a single value")
-    value_set = ["Male", "Female", nothing]
-    @test  onehot("Male", value_set) == [1, 0]
-    @test  onehot("Female", value_set) == [0, 1]
-    @test  onehot(nothing, value_set) == [0, 0]
+
+    @test_throws ErrorException onehot(1, [1, 1, 2, 3])
+    @test_throws ErrorException onehot(0, [1, 2, 3])
+
+    value_set = ["Male", "Female", "Others", nothing, missing]
+    @test onehot("Male", value_set) == [1, 0, 0]
+    @test onehot("Female", value_set) == [0, 1, 0]
+    @test onehot("Others", value_set) == [0, 0, 1]
+    @test onehot(nothing, value_set) == [0, 0, 0]
+    @test onehot(nothing, value_set) == [0, 0, 0]
+
 end
 
 function test_onehot_vector()
-    m = ["Male", "Female", nothing]
-    expected = [1 0
-                0 1
-                0 0]
+    println("-- Testing onehot encoding of a categorical vector")
+
+    m = ["Male", "Female", "Others", nothing, missing]
+    expected = [1 0 0
+                0 1 0
+                0 0 1
+                0 0 0
+                0 0 0]
     @test onehot(m) == expected
 end
 
