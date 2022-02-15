ece6576a7281f6443a960658f8629945d0fd5e52
Takuya Kitazawa <k.takuti@gmail.com>
2022-02-15 07:47:42 -0800

    Refactor synthetic feature/rule interfaces with custom sampler
    https://docs.julialang.org/en/v1/stdlib/Random/#Custom-sampler-types

--- src/synthetic.jl
+++ src/synthetic.jl
@@ -1,35 +1,52 @@
-export Feature, Rule, accumulate, generate
+export SyntheticFeature, SyntheticRule, accumulate, generate
 
-struct Feature
+struct SyntheticFeature
     name::String
-    default::Any
-    random::Function
+    candidates::AbstractVector{Union{String, Infinite}}
+    value::Union{String, Infinite}
+
+    function SyntheticFeature(name::String, candidates::UnitRange)
+        new(name, collect(candidates), -1)
+    end
+
+    function SyntheticFeature(name::String, candidates::AbstractVector)
+        new(name, candidates, -1)
+    end
+
+    function SyntheticFeature(name::String, candidates::AbstractVector, value::Union{String, Infinite})
+        new(name, candidates, value)
+    end
 end
 
-struct Rule
+Random.rand(rng::AbstractRNG, f::Random.SamplerTrivial{SyntheticFeature}) =
+    SyntheticFeature(f[].name, f[].candidates, rand(rng, f[].candidates))
+
+struct SyntheticRule
     # return bool
-    f::Function
+    is_match::Function
 
     # if f returns true, accumulative CTR is lifted p%
-    p::Float64
+    prob::Float64
 end
 
-function accumulate(sample::Dict, rules::Array{Rule,1})
+function accumulate(sample::Dict{String, Any}, rules::AbstractVector{SyntheticRule})
     ctr = 0.0
     for rule in rules
-        if rule.f(sample)
-            ctr += rule.p
+        if rule.is_match(sample)
+            ctr += rule.prob
         end
     end
     ctr
 end
 
-function generate(sample::Dict, rules::Array{Rule,1})
-    rand() <= accumulate(sample, rules)
-end
-
-function generate(samples::Array{Dict,1}, rules::Array{Rule,1})
-    map(samples) do sample
-        generate(sample, rules)
+function generate(n_samples::Integer, features::AbstractVector{SyntheticFeature}, rules::AbstractVector{SyntheticRule})
+    samples = hcat(map(f ->  rand(f, n_samples), features)...) # n_samples * len(features) matrix
+    feedback = Bool[]
+    for sample in eachrow(samples)
+        push!(
+            feedback,
+            rand() <= accumulate(Dict(map(f -> (f.name, f.value), sample)), rules)
+        )
     end
+    feedback
 end
