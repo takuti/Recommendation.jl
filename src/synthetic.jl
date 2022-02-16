45f005e791a6cff12e406e340592c686c2b48eaf
Takuya Kitazawa <k.takuti@gmail.com>
2022-02-16 06:29:22 -0800

    Decouple synthetic item id from matching func in rule
    More tightly aligned with the original paper:
    https://arxiv.org/pdf/1308.1792.pdf (Sec. 7.3)

--- src/synthetic.jl
+++ src/synthetic.jl
@@ -22,30 +22,33 @@ Random.rand(rng::AbstractRNG, f::Random.SamplerTrivial{SyntheticFeature}) =
     SyntheticFeature(f[].name, f[].candidates, rand(rng, f[].candidates))
 
 struct SyntheticRule
+    # item ID
+    item::Union{Nothing, Integer}
+
     # return bool
-    is_match::Function
+    match::Function
 
     # if f returns true, accumulative CTR is lifted p%
-    prob::Float64
+    probability::Float64
 end
 
-function accumulate(sample::Dict{String, Any}, rules::AbstractVector{SyntheticRule})
-    ctr = 0.0
+function accumulate(item::Integer, sample::Dict{String, Any}, rules::AbstractVector{SyntheticRule})
+    cumulative_probability = 0.0
     for rule in rules
-        if rule.is_match(sample)
-            ctr += rule.prob
+        if (isnothing(rule.item) || rule.item == item) && rule.match(sample)
+            cumulative_probability += rule.probability
         end
     end
-    ctr
+    cumulative_probability
 end
 
-function generate(n_samples::Integer, features::AbstractVector{SyntheticFeature}, rules::AbstractVector{SyntheticRule})
+function generate(n_samples::Integer, n_items::Integer, features::AbstractVector{SyntheticFeature}, rules::AbstractVector{SyntheticRule})
     samples = hcat(map(f ->  rand(f, n_samples), features)...) # n_samples * len(features) matrix
     feedback = Bool[]
     for sample in eachrow(samples)
         push!(
             feedback,
-            rand() <= accumulate(Dict(map(f -> (f.name, f.value), sample)), rules)
+            rand() <= accumulate(rand(1:n_items), Dict(map(f -> (f.name, f.value), sample)), rules)
         )
     end
     feedback
--- test/test_synthetic.jl
+++ test/test_synthetic.jl
@@ -5,31 +5,31 @@ features = SyntheticFeature[]
 
 push!(features, SyntheticFeature("Age", 1930:2010))
 push!(features, SyntheticFeature("Geo", ["Arizona", "California", "Colorado", "Illinois", "Indiana", "Michigan", "New York", "Utah"]))
-push!(features, SyntheticFeature("Ad", 0:4))
 
 # create a set of rules
 rules = SyntheticRule[]
 
-push!(rules, SyntheticRule(s -> true, 0.001))
-push!(rules, SyntheticRule(s -> s["Ad"] == 2, 0.01))
-push!(rules, SyntheticRule(s -> s["Age"] >= 1980 && s["Age"] <= 1989 && s["Geo"] == "New York" && s["Ad"] == 0, 0.30))
-push!(rules, SyntheticRule(s -> s["Age"] >= 1950 && s["Age"] <= 1959 && s["Geo"] == "New York" && s["Ad"] == 1, 0.30))
-push!(rules, SyntheticRule(s -> s["Age"] >= 1980 && s["Age"] <= 1989 && s["Geo"] == "Arizona" && s["Ad"] == 1, 0.30))
-push!(rules, SyntheticRule(s -> s["Age"] >= 1950 && s["Age"] <= 1959 && s["Geo"] == "Arizona" && s["Ad"] == 0, 0.30))
+push!(rules, SyntheticRule(nothing, s -> true, 0.001))
+push!(rules, SyntheticRule(3, s -> true, 0.01))
+push!(rules, SyntheticRule(1, s -> s["Age"] >= 1980 && s["Age"] <= 1989 && s["Geo"] == "New York", 0.30))
+push!(rules, SyntheticRule(2, s -> s["Age"] >= 1950 && s["Age"] <= 1959 && s["Geo"] == "New York", 0.30))
+push!(rules, SyntheticRule(2, s -> s["Age"] >= 1980 && s["Age"] <= 1989 && s["Geo"] == "Arizona", 0.30))
+push!(rules, SyntheticRule(1, s -> s["Age"] >= 1950 && s["Age"] <= 1959 && s["Geo"] == "Arizona", 0.30))
 
 # check if a accumulative CTR is computed correctly
-sample1 = Dict("Age" => 1940, "Geo" => "California", "Ad" => 0)
-@test Recommendation.accumulate(sample1, rules) == 0.001
+sample1 = Dict("Age" => 1940, "Geo" => "California")
+@test Recommendation.accumulate(1, sample1, rules) == 0.001
 
-sample2 = Dict("Age" => 1940, "Geo" => "California", "Ad" => 2)
-@test Recommendation.accumulate(sample2, rules) == 0.011
+sample2 = Dict("Age" => 1940, "Geo" => "California")
+@test Recommendation.accumulate(3, sample2, rules) == 0.011
 
-sample3 = Dict("Age" => 1953, "Geo" => "New York", "Ad" => 1)
-@test Recommendation.accumulate(sample3, rules) == 0.301
+sample3 = Dict("Age" => 1953, "Geo" => "New York")
+@test Recommendation.accumulate(2, sample3, rules) == 0.301
 
 # generate samples with random pairs of demographics and ad variants
 n_samples = 100
-feedback = generate(n_samples, features, rules)
+n_items = 5
+feedback = generate(n_samples, n_items, features, rules)
 
 @test isa(feedback, Array{Bool})
 @test length(feedback) == n_samples
