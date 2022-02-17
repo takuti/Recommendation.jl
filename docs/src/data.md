72e47ebb8c8bbfe0f59f0d47c6ff522bece180ba
Takuya Kitazawa <k.takuti@gmail.com>
2022-02-17 07:01:07 -0800

    Add dedicated doc page for data prep

---
+++ docs/src/data.md
@@ -0,0 +1,40 @@
+# Preparing Data
+
+## Onehot encoding
+
+```@docs
+onehot
+```
+
+## Load public datasets
+
+```@docs
+load_movielens_100k
+load_movielens_latest
+load_amazon_review
+load_lastfm
+```
+
+Test a recommender with `cross_validation`:
+
+```julia
+using Recommendation
+
+data = load_movielens_100k()
+recall = cross_validation(
+                          1,            # N-fold
+                          Recall,       # Metric
+                          5,            # Top-k
+                          MostPopular,  # Recommender
+                          data          # Data Accessor
+                         )
+println(recall)
+```
+
+## Generate synthetic data
+
+```@docs
+SyntheticFeature
+SyntheticRule
+generate
+```
--- src/synthetic.jl
+++ src/synthetic.jl
@@ -1,5 +1,22 @@
 export SyntheticFeature, SyntheticRule, accumulate, generate
 
+"""
+    SyntheticFeature(name::String, candidates::Union{UnitRange, AbstractVector})
+
+Synthetic feature generator that allows us to sample a value from `candidates`.
+
+```julia
+features = SyntheticFeature[]
+
+age = SyntheticFeature("Age", 1930:2010))
+push!(features, age)
+
+geo = SyntheticFeature("Geo", ["Arizona", "California", "Colorado", "Illinois", "Indiana", "Michigan", "New York", "Utah"])
+push!(features, geo)
+
+rand(geo, 3) # e.g., ["California", "New York", "Arizona"]
+```
+"""
 struct SyntheticFeature
     name::String
     candidates::Union{UnitRange, AbstractVector}
@@ -25,6 +42,24 @@ to_dict(features::AbstractVector{SyntheticFeature}) = Dict(map(feature -> (featu
 Random.rand(rng::AbstractRNG, f::Random.SamplerTrivial{SyntheticFeature}) =
     SyntheticFeature(f[].name, f[].candidates, rand(rng, f[].candidates))
 
+"""
+    SyntheticRule{probability::Float64[, item::Union{Nothing, Integer}, match::Function])
+
+Matching rule for cumulative "click through rate". Given an item index,
+we increase the probability of acceptance upon sampling when `match` returns `true`,
+which takes a dictionary of feature name => value.
+
+```julia
+rules = SyntheticRule[]
+
+push!(rules, SyntheticRule(0.001))
+push!(rules, SyntheticRule(0.01, 3))
+push!(rules, SyntheticRule(0.30, 1, s -> s["Age"] >= 1980 && s["Age"] <= 1989 && s["Geo"] == "New York"))
+push!(rules, SyntheticRule(0.30, 2, s -> s["Age"] >= 1950 && s["Age"] <= 1959 && s["Geo"] == "New York"))
+push!(rules, SyntheticRule(0.30, 2, s -> s["Age"] >= 1980 && s["Age"] <= 1989 && s["Geo"] == "Arizona"))
+push!(rules, SyntheticRule(0.30, 1, s -> s["Age"] >= 1950 && s["Age"] <= 1959 && s["Geo"] == "Arizona"))
+```
+"""
 struct SyntheticRule
     # if f returns true, accumulative CTR is lifted p%
     probability::Float64
@@ -60,6 +95,25 @@ function accumulate(item::Integer, sample::Dict{String, Any}, rules::AbstractVec
     cumulative_probability
 end
 
+"""
+    generate(n_samples::Integer, n_items::Integer, features::AbstractVector{SyntheticFeature}, rules::AbstractVector{SyntheticRule}) -> DataAccessor
+
+Generate a synthetic data accessor from randomly sampled implicit feedback.
+Each sample is considered as a different user, and user attributes are represented by
+a numeric onehot-encoded feature vector based on the values returned by `SyntheticFeature`.
+
+The process is based on Section 7.3 of the following paper:
+
+- M. Aharon, et al.
+  **OFF-Set: One-pass Factorization of Feature Sets for Online Recommendation in Persistent Cold Start Settings**.
+  [arXiv:1308.1792](https://arxiv.org/abs/1308.1792).
+
+```julia
+n_samples = 256
+n_items = 5
+data = generate(n_samples, n_items, features, rules)
+```
+"""
 function generate(n_samples::Integer, n_items::Integer, features::AbstractVector{SyntheticFeature}, rules::AbstractVector{SyntheticRule})
     events = Event[]
     attributes = Dict()
