23fbba21e1c79da419fdd1e70d6c06dd7d09d1ba
Takuya Kitazawa <k.takuti@gmail.com>
2022-03-08 08:01:26 -0800

    Document non-accuracy metrics

--- docs/src/evaluation.md
+++ docs/src/evaluation.md
@@ -30,3 +30,24 @@ ReciprocalRank
 MPR
 NDCG
 ```
+
+## Aggregated metrics
+
+Return a single aggregated score for an array of multiple top-``k`` recommended items. [Recommender Systems Handbook](https://www.bgu.ac.il/~shanigu/Publications/EvaluationMetrics.17.pdf). gives an overview of such aggregated metrics. In particular, the formulation of Gini index and Shannon Entropy can be found at Eq. (20) and (21) on page 26.
+
+```@docs
+AggregatedDiversity
+ShannonEntropy
+GiniIndex
+```
+
+## Intra-list metrics
+
+Given a list of recommended items (for a single user), intra-list metrics quantifies the quality of the recommendation list from a non-accuracy perspective. [A Survey of Serendipity in Recommender Systems](https://dl.acm.org/doi/10.1016/j.knosys.2016.08.014) highlights the foundation of these metrics.
+
+```@docs
+Coverage
+Novelty
+IntraListSimilarity
+Serendipity
+```
--- src/metrics/aggregated.jl
+++ src/metrics/aggregated.jl
@@ -1,20 +1,64 @@
 export AggregatedDiversity, ShannonEntropy, GiniIndex
 export find_all_items, count_users_contain
 
+"""
+    find_all_items(recommendations::AbstractVector{<:AbstractVector{<:Integer}}) -> Set
+
+Return a set of distinct items from an array of multiple top-k recommendation lists.
+"""
 function find_all_items(recommendations::AbstractVector{<:AbstractVector{<:Integer}})
     Set(reduce(vcat, recommendations))
 end
 
+"""
+    AggregatedDiversity
+
+The number of distinct items recommended across all suers. Larger value indicates more diverse recommendation result overall.
+
+```julia
+measure(
+    metric::AggregatedDiversity, recommendations::AbstractVector{<:AbstractVector{<:Integer}}
+)
+```
+
+Let ``U`` and ``I`` be a set of users and items, respectively, and ``L_N(u)`` a list of top-``N`` recommended items for a user ``u``. Here, an aggregated diversity can be calculated as:
+
+```math
+\\left| \\bigcup\\limits_{u \\in U} L_N(u) \\right|
+```
+"""
 struct AggregatedDiversity <: AggregatedMetric end
 function measure(metric::AggregatedDiversity, recommendations::AbstractVector{<:AbstractVector{<:Integer}})
     # number of distinct items recommended across all users
     length(find_all_items(recommendations))
 end
 
+"""
+    count_users_contain(item::Integer, recommendations::AbstractVector{<:AbstractVector{<:Integer}}) -> Int
+
+Given an array of top-k recommendation lists for multiple users, count the number of users who are recommended a particular item `item`.
+"""
 function count_users_contain(item::Integer, recommendations::AbstractVector{<:AbstractVector{<:Integer}})
     sum(map(recs -> (item in recs), recommendations))
 end
 
+"""
+    ShannonEntropy
+
+If we focus more on individual items and how many users are recommended a particular item, the diversity of top-`k` recommender can be defined by [Shannon Entropy](https://en.wikipedia.org/wiki/Entropy_(information_theory)):
+
+```math
+-\\sum_{j = 1}^{|I|} \\left( \\frac{\\left|\\{u \\mid u \\in U \\wedge i_j \\in L_N(u) \\}\\right|}{N |U|} \\ln \\left( \\frac{\\left|\\{u \\mid u \\in U \\wedge i_j \\in L_N(u) \\}\\right|}{N |U|}  \\right) \\right)
+```
+where ``i_j`` denotes ``j``-th item in the available item set ``I``.
+
+```julia
+measure(
+    metric::ShannonEntropy, recommendations::AbstractVector{<:AbstractVector{<:Integer}};
+    k::Integer
+)
+```
+"""
 struct ShannonEntropy <: AggregatedMetric end
 function measure(metric::ShannonEntropy, recommendations::AbstractVector{<:AbstractVector{<:Integer}}; k::Integer)
     n_users = size(recommendations, 1)
@@ -27,6 +71,24 @@ function measure(metric::ShannonEntropy, recommendations::AbstractVector{<:Abstr
     -entropy
 end
 
+"""
+    GiniIndex
+
+[Gini Index](https://en.wikipedia.org/wiki/Gini_coefficient), which is normally used to measure a degree of inequality in a distribution of income, can be applied to assess diversity in the context of top-`k` recommendation:
+
+```math
+\\frac{1}{|I| - 1} \\sum_{j = 1}^{|I|} \\left( (2j - |I| - 1) \\cdot \\frac{\\left|\\{u \\mid u \\in U \\wedge i_j \\in L_N(u) \\}\\right|}{N |U|} \\right)
+```
+
+```julia
+measure(
+    metric::GiniIndex, recommendations::AbstractVector{<:AbstractVector{<:Integer}};
+    k::Integer
+)
+```
+
+The index is 0 when all items are equally chosen in terms of the number of recommended users.
+"""
 struct GiniIndex <: AggregatedMetric end
 function measure(metric::GiniIndex, recommendations::AbstractVector{<:AbstractVector{<:Integer}}; k::Integer)
     n_users = size(recommendations, 1)
