3f15d34d4f283fc3189612ee229b4fdc9f7e46b4
Takuya Kitazawa <k.takuti@gmail.com>
2022-10-20 11:13:26 -0700

    Benchmark with distinct pairs of recommender-metrics
    * Each recommender chooses if it uses ranking or accuracy metrics, or
      both.
    * Evaluate mutiple metrics at once with `evaluate(...,
      metrics::AbstractArray[<:Metrics], ...)`

--- examples/benchmark.jl
+++ examples/benchmark.jl
@@ -1,11 +1,12 @@
 using Recommendation
 
-recommenders = Dict(
+recommenders = [
+    # (recommender => params, accuracy_metrics, ranking_metrics)
+    (ItemMean => [], true, true),
+    (MostPopular => [], false, true),
+    (ThresholdPercentage => [3.0], false, true),
+    (UserMean => [], true, true),
     # CoOccurrence => [1],
-    ItemMean => [],
-    # MostPopular => [],
-    # ThresholdPercentage => [3.0],
-    UserMean => [],
     # BPRMatrixFactorization => [],
     # FactorizationMachines => [],
     # ItemKNN => [5],
@@ -13,25 +14,31 @@ recommenders = Dict(
     # SVD => [],
     # TfIdf => [],
     # UserKNN => [5, true]
-)
+]
 
-metrics = [
+accuracy_metrics = [
     RMSE,
     MAE,
-    # AggregatedDiversity,
-    # ShannonEntropy,
-    # GiniIndex,
-    # Coverage,
-    # Novelty,
-    # IntraListSimilarity,
-    # Serendipity,
-    # Recall,
-    # Precision,
-    # AUC,
-    # ReciprocalRank,
-    # MPR,
-    # NDCG
 ]
+
+ranking_metrics = [
+    Recall,
+    Precision,
+    AUC,
+    ReciprocalRank,
+    MPR,
+    NDCG
+]
+
+# metrics = [
+#     AggregatedDiversity,
+#     ShannonEntropy,
+#     GiniIndex,
+#     Coverage,
+#     Novelty,
+#     IntraListSimilarity,
+#     Serendipity,
+# ]
 
 datasets = [
     load_movielens_100k,
@@ -47,21 +54,25 @@ for dataset in datasets
     @info "Dataset: $dataset"
     data = dataset()
     train_data, truth_data = split_data(data, test_ratio)
-    for (recommender, params) in recommenders
+    for ((recommender, params), use_accuracy_metrics, use_ranking_metrics) in recommenders
         @info "Recommender: $recommender"
         r = recommender(train_data, params...)
         fit!(r)
 
         # accuracy metrics
-        results = evaluate(r, truth_data, [metric() for metric in accuracy_metrics])
-        for (metric, res) in zip(accuracy_metrics, results)
-            @info "$metric = $res"
+        if use_accuracy_metrics
+            results = evaluate(r, truth_data, [metric() for metric in accuracy_metrics])
+            for (metric, res) in zip(accuracy_metrics, results)
+                @info "$metric = $res"
+            end
         end
 
         # ranking metrics
-        results = evaluate(r, truth_data, [metric() for metric in ranking_metrics], topk)
-        for (metric, res) in zip(ranking_metrics, results)
-            @info "$metric = $res"
+        if use_ranking_metrics
+            results = evaluate(r, truth_data, [metric() for metric in ranking_metrics], topk)
+            for (metric, res) in zip(ranking_metrics, results)
+                @info "$metric = $res"
+            end
         end
     end
 end
