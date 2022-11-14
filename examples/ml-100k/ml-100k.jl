62a3ce3cf501c3dc05d412ded6d5ec6738765768
Takuya Kitazawa <k.takuti@gmail.com>
2022-11-14 11:29:14 -0800

    Activate pkg if needed, by checking ML100k code execution path

--- examples/ml-100k/ml-100k.jl
+++ examples/ml-100k/ml-100k.jl
@@ -1,3 +1,18 @@
+# If this script is executed directly from the Recommendation.jl repository,
+# instantiate the package from the local code so everything is up-to-date.
+# Otherwise, we assume Recommendation.jl is pre-installed, and
+# `using Recommendation` works without manual activation.
+proj_toml_path = normpath(joinpath(@__DIR__, "..", "..", "Project.toml"))
+if isfile(proj_toml_path)
+    using TOML
+    conf = TOML.parse(read(proj_toml_path, String))
+    if conf["name"] == "Recommendation"
+        using Pkg
+        Pkg.activate(dirname(proj_toml_path))
+        Pkg.instantiate()
+    end
+end
+
 using Recommendation
 
 # configureable parameters for cross validation
