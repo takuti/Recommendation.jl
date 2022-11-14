20fb016b6621c11b98a42046eda3ab992c2cf1db
Takuya Kitazawa <k.takuti@gmail.com>
2022-11-14 11:22:14 -0800

    Set variables for CV params to make ML100k example configureable

--- examples/ml-100k/ml-100k.jl
+++ examples/ml-100k/ml-100k.jl
@@ -1,11 +1,12 @@
 using Recommendation
 
+# configureable parameters for cross validation
+n_fold = 2
+metric = Recall
+topk = 5
+recommender = MostPopular
 data = load_movielens_100k()
-recall = cross_validation(
-                          1,            # N-fold
-                          Recall,       # Metric
-                          5,            # Top-k
-                          MostPopular,  # Recommender
-                          data          # Data Accessor
-                         )
-println(recall)
+
+res = cross_validation(n_fold, metric, topk,
+                       recommender, data)
+println("Average $metric = $res")
