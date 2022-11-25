ec030cdca754c79ffa8a1f89ebefebc3f3b5cb36
Takuya Kitazawa <k.takuti@gmail.com>
2022-11-25 10:47:54 -0800

    Let cross validators take instantiated metric(s) as argument
    because metrics don't need to be instantiated for every validation fold
    unlike recommender instance.

--- test/evaluation/test_cross_validation.jl
+++ test/evaluation/test_cross_validation.jl
@@ -6,29 +6,29 @@ function test_cross_validation_accuracy(v)
     data = DataAccessor(isa(v, Unknown) ? m : sparse(m))
 
     # 1-fold cross validation is invalid
-    @test_throws ErrorException cross_validation(1, MAE, MF, data, 2)
+    @test_throws ErrorException cross_validation(1, MAE(), MF, data, 2)
 
     # in n-fold cross validation, n must be smaller than or equal to the number of all samples
-    @test_throws ErrorException cross_validation(100, MAE, MF, data, 2)
+    @test_throws ErrorException cross_validation(100, MAE(), MF, data, 2)
 
     fold = 5
 
     # MF(data, 2)
-    @test 0.0 < cross_validation(fold, MAE, MF, data, 2) <= 2.5
+    @test 0.0 < cross_validation(fold, MAE(), MF, data, 2) <= 2.5
 
     # UserMean(data)
-    @test 0.0 < cross_validation(fold, MAE, UserMean, data) <= 2.5
+    @test 0.0 < cross_validation(fold, MAE(), UserMean, data) <= 2.5
 
     # ItemMean(data)
-    @test 0.0 < cross_validation(fold, MAE, ItemMean, data) <= 2.5
+    @test 0.0 < cross_validation(fold, MAE(), ItemMean, data) <= 2.5
 
     # UserKNN(data, 2, true)
-    @test 0.0 < cross_validation(fold, MAE, UserKNN, data, 2, true) <= 2.5
+    @test 0.0 < cross_validation(fold, MAE(), UserKNN, data, 2, true) <= 2.5
 
     # leave-one-out cross validation with MF(data, 2)
     n_samples = length(data.events)
-    @test 0.0 < cross_validation(n_samples, MAE, MF, data, 2) <= 2.5
-    @test 0.0 < leave_one_out(MAE, MF, data, 2) <= 2.5
+    @test 0.0 < cross_validation(n_samples, MAE(), MF, data, 2) <= 2.5
+    @test 0.0 < leave_one_out(MAE(), MF, data, 2) <= 2.5
 end
 
 function test_cross_validation_ranking(v)
@@ -41,27 +41,27 @@ function test_cross_validation_ranking(v)
     k = 4
 
     # 1-fold cross validation is invalid
-    @test_throws ErrorException cross_validation(1, Recall, k, MF, data, 2)
+    @test_throws ErrorException cross_validation(1, Recall(), k, MF, data, 2)
 
     # in n-fold cross validation, n must be smaller than or equal to the number of all samples
-    @test_throws ErrorException cross_validation(100, Recall, k, MF, data, 2)
+    @test_throws ErrorException cross_validation(100, Recall(), k, MF, data, 2)
 
     # 3-fold cross validation
     fold = 3
 
     # MF(data, 2)
-    @test 0.0 <= cross_validation(fold, Recall, k, MF, data, 2) <= 1.0
+    @test 0.0 <= cross_validation(fold, Recall(), k, MF, data, 2) <= 1.0
 
     # MostPopular(data)
-    @test 0.0 <= cross_validation(fold, Recall, k, MostPopular, data) <= 1.0
+    @test 0.0 <= cross_validation(fold, Recall(), k, MostPopular, data) <= 1.0
 
     # UserKNN(data, 2, true)
-    @test 0.0 <= cross_validation(fold, Recall, k, UserKNN, data, 2, true) <= 1.0
+    @test 0.0 <= cross_validation(fold, Recall(), k, UserKNN, data, 2, true) <= 1.0
 
     # leave-one-out cross validation with MF(data, 2)
     n_samples = length(data.events)
-    @test 0.0 <= cross_validation(n_samples, Recall, k, MF, data, 2) <= 1.0
-    @test 0.0 <= leave_one_out(Recall, k, MF, data, 2) <= 1.0
+    @test 0.0 <= cross_validation(n_samples, Recall(), k, MF, data, 2) <= 1.0
+    @test 0.0 <= leave_one_out(Recall(), k, MF, data, 2) <= 1.0
 end
 
 println("-- Testing cross validation with accuracy metrics")
